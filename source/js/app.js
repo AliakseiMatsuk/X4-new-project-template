bd7d3849eebc317b829ce2e30395d979f409b78d
Aliaksei <matsukaliaksei@gmail.com>
2017-07-14 10:55:22 +0300

    added jeet + masonry grid system

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,13 @@
 (function() {
     'use strict';
+    $(document).ready(function() {
+        var $grid = $('.grid').imagesLoaded(function() {
+            // init Masonry after all images have loaded
+            $grid.masonry({
+            	itemSelector: '.grid__item', // use a separate class for itemSelector, other than .col-
+            	columnWidth: '.grid-sizer',
+            	percentPosition: true
+            });
+        });
+    });
 })();
